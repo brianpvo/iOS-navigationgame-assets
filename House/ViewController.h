fdd9ffdde619e81526a3c007ef1572112e93bbcd
Brian Vo <brianvo@Brians-MacBook-Pro.local>
2018-04-18 16:53:25 -0700

    started house project with a few view controllers in storyboard

---
+++ House/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  House
+//
+//  Created by Brian Vo on 2018-04-18.
+//  Copyright © 2018 Brian Vo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
